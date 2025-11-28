9a26c0d98a5fedc214d03bd2a1fab06d609c40ae
Matheus <164814274+TeteuX26@users.noreply.github.com>
2025-11-27 22:18:46 -0300

    Banco do site

---
+++ ecoCalc.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE ecoCalc;
+USE ecoCalc;
+
+CREATE TABLE usuario (
+    cod INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(100) NOT NULL,
+    senha VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE conexao (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    cod_user INT NOT NULL,
+    nome VARCHAR(100),
+    descricao VARCHAR(255),
+    FOREIGN KEY (cod_user)
+        REFERENCES usuario (cod)
+);
